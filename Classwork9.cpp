5b23d25066b44d140c22894c61a84d23960d5e29
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-11-24 19:05:55 +0200

    1

--- Classwork9.cpp
+++ Classwork9.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 struct Notebook {
-	struct disp_res {   //resolution of display
-		int x, y;     //horizontal and vertical
+	struct disp_res {   
+		int x, y;     
 	};
 	disp_res l;
-	int f; //frequancy of regeneration
-	float d; //diagonale
-	float hdd;  //capacity
-	char model[21];  //model
-	int price; //price
+	int f; 
+	float d; 
+	float hdd;  
+	char model[21];  
+	int price; 
 	double weight;
 	double length;
 	double width;
 	double height;
-	int cf;   //cpu frequancy
+	int cpu;   
 	int ram;
 	int gpu;
 };
@@ -39,8 +39,8 @@ int main()
 	for (int i = 0; i < n; ++i) 
 	{
 		f.get(mas[i].model, 21);
-		f >> mas[i].price >> mas[i].weight >> mas[i].height >> t >> mas[i].length >> t >> mas[i].width >> mas[i].cf >> mas[i].ram >> mas[i].d >> mas[i].gpu >> mas[i].l.x >> t >> mas[i].l.y >> mas[i].f >> mas[i].hdd;
-		cout << ' ' << mas[i].model << ' ' << mas[i].price << ' ' << "\t" << mas[i].weight << ' ' << "\t" << mas[i].height << '*' <<  mas[i].length << '*' <<  mas[i].width << ' ' << "\t" << mas[i].cf << ' ' << "\t" << mas[i].ram << ' ' << "\t" << mas[i].d << ' ' << "\t" << mas[i].gpu << ' ' << "\t" <<  mas[i].l.x << '*' << mas[i].l.y << ' ' << "\t" << mas[i].f << ' ' << "\t" << mas[i].hdd<<"\t";
+		f >> mas[i].price >> mas[i].weight >> mas[i].height >> t >> mas[i].length >> t >> mas[i].width >> mas[i].cpu >> mas[i].ram >> mas[i].d >> mas[i].gpu >> mas[i].l.x >> t >> mas[i].l.y >> mas[i].f >> mas[i].hdd;
+		cout << ' ' << mas[i].model << ' ' << mas[i].price << ' ' << "\t" << mas[i].weight << ' ' << "\t" << mas[i].height << '*' <<  mas[i].length << '*' <<  mas[i].width << ' ' << "\t" << mas[i].cpu << ' ' << "\t" << mas[i].ram << ' ' << "\t" << mas[i].d << ' ' << "\t" << mas[i].gpu << ' ' << "\t" <<  mas[i].l.x << '*' << mas[i].l.y << ' ' << "\t" << mas[i].f << ' ' << "\t" << mas[i].hdd<<"\t";
 		cout << endl;
 		f.getline(t1, 1);
 	}
@@ -53,7 +53,7 @@ int main()
 	Notebook p;
 	for (int i = 0; i < n; ++i) {
 		for (int j = i + 1; j < n; ++j) {
-			if (mas[j].cf < mas[i].cf ) {
+			if (mas[j].cpu < mas[i].cpu ) {
 				p = mas[i];
 				mas[i] = mas[j];
 				mas[j] = p;
